0a0bed3f9501c4a68981575aa077b78acca56166
Phạm Hải Đức <147225862+phamdocdaoabc@users.noreply.github.com>
2025-02-23 21:30:47 +0700

    Delete ProductService/ProductService directory

--- ProductService/ProductService/src/main/resources/db/migration/V1__create_categories_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE categories (
-                            category_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                            category_name VARCHAR(255),
-                            category_image_url VARCHAR(255),
-
-);
